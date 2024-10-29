85781ef2c49c4d4d91a0cd506eaf9b893a379352
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-10-29 12:57:34 -0500

    added layout

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,26 @@ import Header from '@/components/Header.vue';
 </script>
 
 <template>
-    <Header/>
+    <div class="layout_container grid min-h-dvh">
+        <!-- Header content  -->
+        <div>    
+            <Header/>
+        </div>
+
+        <!-- Main content -->
+        <main>
+        </main>
+        
+        <!-- Footer content -->
+        <footer>
+            
+        </footer>
+    </div>
+    
 </template>
 
 <style scoped>
+.layout_container{
+    grid-template-rows: auto 1fr auto;
+}
 </style>

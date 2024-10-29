4d91542cc4aa4328c19be1f6a02cc0a7cc319643
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-10-29 10:03:48 -0500

    Caracteristicas principales ready

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import Header from '@/components/Header.vue';
+import Header from "@/components/Header.vue";
+import mainFeatures from "@/components/mainFeatures.vue";
 </script>
 
 <template>
-    <Header/>
+  <Header />
+  <div class="mt-5">
+      <mainFeatures />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/mainFeatures.vue
@@ -0,0 +1,159 @@
+<script setup></script>
+
+<template>
+  <div class="bg-[#333333] m-10 rounded-2xl">
+    <div class="container mx-auto">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <!--CARD 1-->
+        <div
+          class="bg-[#333333] rounded-lg p-6 text-left text-white relative mt-4 ml-1"
+        >
+          <div
+            class="feature-icon flex justify-left transform hover:scale-100 transition-transform duration-700 ease-in-out"
+          >
+            <div
+              class="w-20 h-20 bg-[#2ebaa1] rounded-full flex items-center justify-center transform hover:scale-110 transition-transform duration-700 ease-in-out"
+            >
+              <img
+                src="../../public/images/default_image.svg"
+                class="w-10 h-10"
+              />
+            </div>
+          </div>
+          <div class="mb-5">
+            <span
+              class="absolute top-4 right-4 text-5xl font-extrabold block text-transparent"
+              style="
+                -webkit-text-stroke: 2px #2ebaa1;
+                -webkit-text-fill-color: transparent;
+              "
+            >
+              01
+            </span>
+          </div>
+          <div class="font-epilogue">
+            <h4 class="text-[22px] font-extrabold mb-2">Facilidad de Becas</h4>
+            <p class="text-sm leading-[1.8] font-semibold">
+              Accede a nuestras becas y ayudas financieras diseñadas para apoyar
+              tu educación.
+            </p>
+          </div>
+        </div>
+
+        <!--CARD 2-->
+        <div
+          class="bg-[#333333] rounded-lg p-6 text-left text-white relative mt-4"
+        >
+          <div
+            class="feature-icon flex justify-left transform hover:scale-100 transition-transform duration-700 ease-in-out"
+          >
+            <div
+              class="w-20 h-20 bg-[#2ebaa1] rounded-full flex items-center justify-center transform hover:scale-110 transition-transform duration-700 ease-in-out"
+            >
+              <img
+                src="../../public/images/default_image.svg"
+                class="w-10 h-10"
+              />
+            </div>
+          </div>
+          <div class="mb-5">
+            <span
+              class="absolute top-4 right-4 text-5xl font-extrabold block text-transparent"
+              style="
+                -webkit-text-stroke: 2px #2ebaa1;
+                -webkit-text-fill-color: transparent;
+              "
+            >
+              02
+            </span>
+          </div>
+          <div class="font-epilogue">
+            <h4 class="text-[22px] font-extrabold mb-2">
+              Profesores Capacitados
+            </h4>
+            <p class="text-sm leading-[1.8] font-semibold">
+              Aprende de un equipo de profesores comprometidos con brindarte una
+              educación de calidad.
+            </p>
+          </div>
+        </div>
+
+        <!--CARD 3-->
+        <div
+          class="bg-[#333333] rounded-lg p-6 text-left text-white relative mt-4"
+        >
+          <div
+            class="feature-icon flex justify-left transform hover:scale-100 transition-transform duration-700 ease-in-out"
+          >
+            <div
+              class="w-20 h-20 bg-[#2ebaa1] rounded-full flex items-center justify-center transform hover:scale-110 transition-transform duration-700 ease-in-out"
+            >
+              <img
+                src="../../public/images/default_image.svg"
+                class="w-10 h-10"
+              />
+            </div>
+          </div>
+          <div class="mb-5">
+            <span
+              class="absolute top-4 right-4 text-5xl font-extrabold block text-transparent"
+              style="
+                -webkit-text-stroke: 2px #2ebaa1;
+                -webkit-text-fill-color: transparent;
+              "
+            >
+              03
+            </span>
+          </div>
+          <div class="font-epilogue">
+            <h4 class="text-[22px] font-extrabold mb-2">
+              Facilidad de Biblioteca
+            </h4>
+            <p class="text-sm leading-[1.8] font-semibold">
+              Nuestra biblioteca está equipada con una vasta colección de
+              recursos de aprendizaje.
+            </p>
+          </div>
+        </div>
+
+        <!--CARD 4-->
+        <div
+          class="bg-[#333333] rounded-lg p-6 text-left text-white relative mt-4 mr-1"
+        >
+          <div
+            class="feature-icon flex justify-left transform hover:scale-100 transition-transform duration-700 ease-in-out"
+          >
+            <div
+              class="w-20 h-20 bg-[#2ebaa1] rounded-full flex items-center justify-center transform hover:scale-110 transition-transform duration-700 ease-in-out"
+            >
+              <img
+                src="../../public/images/default_image.svg"
+                class="w-10 h-10"
+              />
+            </div>
+          </div>
+          <div class="mb-5">
+            <span
+              class="absolute top-4 right-4 text-5xl font-extrabold block text-transparent"
+              style="
+                -webkit-text-stroke: 2px #2ebaa1;
+                -webkit-text-fill-color: transparent;
+              "
+            >
+              04
+            </span>
+          </div>
+          <div class="font-epilogue">
+            <h4 class="text-[22px] font-extrabold mb-2">Precios Asequibles</h4>
+            <p class="text-sm leading-[1.8] font-semibold">
+              Ofrecemos programas académicos a precios accesibles, para el
+              alcance de todos.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>

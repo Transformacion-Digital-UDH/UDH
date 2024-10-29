9eef58e0bfeb626e0939e6ca421e06294e818f48
xyz <xyzxyzxyzxyz@hotmail.com>
2024-10-29 14:33:53 -0500

    New best model cards

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
-<!-- App.vue -->
 <script setup lang="ts">
-
-import Header from '@/components/Header.vue';
-import DatosUniversidad from '@/components/DatosUniversidad.vue';
-import Carousel from '@/components/Carousel.vue';
+import Header from "@/components/Header.vue";
+import DatosUniversidad from "@/components/DatosUniversidad.vue";
+import Carousel from "@/components/Carousel.vue";
 import mainFeatures from "@/components/mainFeatures.vue";
 import aboutUs from "@/components/aboutUs.vue";
+import CardEvent from "./components/CardEvent.vue";
+</script>
 
 <template>
-    <Header/>
-    <Carousel />
-    <mainFeatures />
-    <aboutUs />
-    <DatosUniversidad />
-<template>
+  <Header />
+  <Carousel />
+  <mainFeatures />
+  <aboutUs />
+  <DatosUniversidad />
+  <CardEvent />
+</template>
 
 <style scoped></style>

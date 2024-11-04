a02ad608e9bfbf0eec1b75c87b12361588ba2aa4
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-11-04 13:38:21 -0500

    Miss responsive in videoCareer

--- src/components/GridInformation.vue
+++ src/components/GridInformation.vue
@@ -1,15 +1,28 @@
+<script setup>
+import videoSistemas from "@/components/CarreraSistemas/videoSistemas.vue";
+</script>
+
 <template>
   <div class="max-w-7xl mx-auto p-4 lg:grid lg:grid-cols-3 gap-4 mb-10">
     <!-- Tarjeta logo responsive -->
-    <div class="min-h-[120px] rounded-xl shadow-custom-card bg-red-100 mb-4 block lg:hidden"></div>
+    <div
+      class="min-h-[120px] rounded-xl shadow-custom-card bg-red-100 mb-4 block lg:hidden"
+    ></div>
 
     <!-- Columna Izquierda (Contenido Principal) -->
-    <div class="lg:col-span-2 min-h-[1200px] rounded-xl shadow-custom-card bg-white"></div>
+    <div
+      class="lg:col-span-2 min-h-[1200px] rounded-xl shadow-custom-card bg-white"
+    >
+      <!--ACA IRA MI VIDEO SISTEMAS-->
+      <videoSistemas />
+    </div>
 
     <!-- Columna Derecha (Tarjetas de Información) -->
     <div class="space-y-4 md:mt-4 lg:mt-0 sm:mt-4 xs:mt-4 xl:mt-0">
       <!-- Tarjeta logo -->
-      <div class="min-h-[120px] rounded-xl shadow-custom-card bg-red-100 hidden lg:block"></div>
+      <div
+        class="min-h-[120px] rounded-xl shadow-custom-card bg-red-100 hidden lg:block"
+      ></div>
 
       <!-- Tarjeta info carrera -->
       <div class="min-h-[450px] rounded-xl shadow-custom-card bg-white"></div>
@@ -23,8 +36,4 @@
   </div>
 </template>
 
-<script setup>
-</script>
-
-<style scoped>
-</style>
+<style scoped></style>

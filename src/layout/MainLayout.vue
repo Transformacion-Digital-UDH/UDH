2bb1bccf075ca6ccfc3105785a2717a750cfbb83
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-04 00:53:57 -0500

    creando layout principal y vistas

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import ButtonMultiple from "@/components/ButtonMultiple.vue";
+
+</script>
+<template>
+    <Header />
+    <slot />
+    <Footer />
+    <ButtonMultiple />
+</template>

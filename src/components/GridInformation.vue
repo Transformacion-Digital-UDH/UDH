3472f2f66404e735a2fef00e8a35d613f7df261c
jeanbecerra <j.becerra.22.jhire@gmail.com>
2024-11-05 12:29:37 -0500

    logo information careers

--- src/components/GridInformation.vue
+++ src/components/GridInformation.vue
@@ -3,12 +3,15 @@ import VideoCareer from "@/components/Carreras/videoCareer.vue";
 import courseRequirement from "@/components/Carreras/courseRequirement.vue";
 import characterCareer from "@/components/Carreras/characterCareer.vue";
 import InfoCareer from "@/components/Carreras/infoCareer.vue";
+import logoCareer from "@/components/Carreras/logoCareer.vue";
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto p-4 lg:grid lg:grid-cols-3 gap-4 mb-10">
     <!-- Tarjeta logo responsive -->
-    <div class="min-h-[120px] rounded-xl shadow-custom-card bg-red-100 mb-4 block lg:hidden"></div>
+    <div class="min-h-[120px] rounded-xl shadow-custom-card bg-white mb-4 block lg:hidden">
+      <logoCareer/>
+    </div>
 
     <!-- Columna Izquierda (Contenido Principal) -->
     <div class="lg:col-span-2 min-h-[1200px] rounded-xl shadow-custom-card bg-white">
@@ -19,7 +22,9 @@ import InfoCareer from "@/components/Carreras/infoCareer.vue";
     <!-- Columna Derecha (Tarjetas de Información) -->
     <div class="space-y-4 md:mt-4 lg:mt-0 sm:mt-4 xs:mt-4 xl:mt-0">
       <!-- Tarjeta logo -->
-      <div class="min-h-[120px] rounded-xl shadow-custom-card bg-red-100 hidden lg:block"></div>
+      <div class="min-h-[120px] rounded-xl shadow-custom-card bg-white hidden lg:block">
+        <logoCareer/>
+      </div>
 
       <!-- Tarjeta info carrera -->
       <div class="min-h-[450px] rounded-xl shadow-custom-card bg-white">

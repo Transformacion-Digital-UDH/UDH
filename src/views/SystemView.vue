0c39584a8b0d682ec847d115b2e673a8749670e4
Renzo <renzopanduro00@gmail.com>
2024-11-05 22:45:19 -0500

    carousel career component

--- src/views/SystemView.vue
+++ src/views/SystemView.vue
@@ -5,15 +5,23 @@ import Carousel from "@/components/Carousel.vue";
 
 const carouselSlides = [
   {
-    backgroundImage: "/imagesSlider/1.jpg",
-    title: "Transforma tu vida con una",
-    highlightedText: "educación de calidad",
+    backgroundImage: "/imagesSlider/proyecto1.jpg",
+    title: "Optimiza tu vida con nuestras soluciones de ",
+    highlightedText: "Internet de las Cosas(IoT)",
     description:
       "En la UDH, creemos en el poder de alcanzar tus metas y a convertirte en un líder en tu campo.",
     buttonLabel: "Conoce más",
   },
   {
-    backgroundImage: "/imagesSlider/2.jpg",
+    backgroundImage: "/imagesSlider/proyecto2.jpg",
+    title: "Impulsa el futuro con",
+    highlightedText: "investigación pionera",
+    description:
+      "En la Universidad de Innovación, nos dedicamos a desarrollar investigaciones que cambian el mundo.",
+    buttonLabel: "Conoce más",
+  },
+  {
+    backgroundImage: "/imagesSlider/proyecto3.jpg",
     title: "Impulsa el futuro con",
     highlightedText: "investigación pionera",
     description:

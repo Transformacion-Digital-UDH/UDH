bfb44bfb4a4ceaf0f73fffa37c358c682700d2ed
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-07 22:57:22 -0500

    configurando como loop infinito

--- src/components/FriendPagesCarousel.vue
+++ src/components/FriendPagesCarousel.vue
@@ -1,13 +1,13 @@
 <template>
-    <section class="w-full h-auto bg-gray-100 py-10">
-      <Splide :options="carouselOptions" class="overflow-hidden xl:mx-60 lg:mx-60 md:mx-32 sm:mx-20 mx-6 ">
-        <SplideSlide v-for="(page, index) in pages" :key="index" class="!w-[150px]">
-          <a :href="page.url" target="_blank" rel="noopener noreferrer">
-            <img :src="page.image" :alt="page.name" class="rounded-lg shadow-lg" />
-          </a>
-        </SplideSlide>
-      </Splide>
-    </section>
+  <section class="w-full h-auto bg-gray-100 py-10">
+    <Splide :options="carouselOptions" class="overflow-hidden xl:mx-60 lg:mx-60 md:mx-32 sm:mx-20 mx-6 ">
+      <SplideSlide v-for="(page, index) in pages" :key="index" class="!w-[150px]">
+        <a :href="page.url" target="_blank" rel="noopener noreferrer">
+          <img :src="page.image" :alt="page.name" class="rounded-lg shadow-lg" />
+        </a>
+      </SplideSlide>
+    </Splide>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -16,29 +16,33 @@ import '@splidejs/vue-splide/css';
 import { PropType } from 'vue';
 
 const props = defineProps({
-    pages: {
-        type: Array as PropType<Array<{ name: string; url: string; image: string }>>,
-        required: true,
-    },
+  pages: {
+      type: Array as PropType<Array<{ name: string; url: string; image: string }>>,
+      required: true,
+  },
 });
 
 const carouselOptions = {
-    type: 'loop',
-    autoplay: true,
-    arrows: false,
-    interval: 1500,
-    pauseOnHover: true,
-    pagination: false,
-    perPage: 5, 
-    gap: '1.5rem',
-    breakpoints: {
-        360: { perPage: 2 },
-        640: { perPage: 2 },
-        768: { perPage: 2 },
-        1024: { perPage: 3 },
-        1280: { perPage: 4 },
-        1536: { perPage: 4 },
-    },
+  type: 'loop',
+  autoplay: true,
+  speed: 10000,
+  interval: 1000,
+  arrows: false,
+  pauseOnHover: true,
+  pagination: false,
+  perPage: 5, 
+  perMove: 1, 
+  rewind: true,
+  gap: '1.5rem',
+  transition: "",
+  breakpoints: {
+      360: { perPage: 2 },
+      640: { perPage: 2 },
+      768: { perPage: 2 },
+      1024: { perPage: 3 },
+      1280: { perPage: 4 },
+      1536: { perPage: 5 },
+  },
 };
 </script>
 

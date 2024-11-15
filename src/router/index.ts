b5421a435d21fb87393fa869696c848b962e1149
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-15 04:24:55 -0500

    cambiando de mainview a homeview y agregando helper que recupera data de strapi api

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
-import MainView from '@/views/MainView.vue'
+import HomeView from '@/views/HomeView.vue'
 import SystemView from '@/views/SystemView.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: MainView },
+  { path: '/', name: 'home', component: HomeView },
   { path: '/carrera/sistemas-e-informatica', name: 'sistemas',  component: SystemView },
 ]
 

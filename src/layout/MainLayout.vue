efcdb343b4b222472245b119a9f88e6baf373e51
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-16 14:15:40 -0500

    adaptando layout para recibir info por componete

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -2,36 +2,10 @@
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import ButtonMultiple from "@/components/ButtonMultiple.vue";
-import { getSettingsPage } from "@/lib/get-settings-page";
-import { onMounted, ref } from "vue";
-
-const redes_sociales = ref([]);
-const contactos = ref([]);
-const logoudh = ref(null);
-const link_login = ref(null);
-
-const fetchSettings = async () => {
-    try {
-        const settingsPage = await getSettingsPage();
-
-        redes_sociales.value = settingsPage.redes_sociales || [];
-        contactos.value = settingsPage.contactos || [];
-        logoudh.value = settingsPage.logoudh || null;
-        link_login.value = settingsPage.link_login || null;
-    } catch (error) {
-        console.error("Error fetching settings data:", error);
-    }
-};
-
-onMounted(fetchSettings);
 
 </script>
 <template>
-    <Header 
-        :redes_sociales="redes_sociales"
-        :logoudh="logoudh!"
-        :link_login="link_login!"
-    />
+    <Header />
     <slot />
     <Footer />
     <ButtonMultiple />

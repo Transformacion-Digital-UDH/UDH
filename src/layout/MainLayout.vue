f9da6c530a9871075de8b31bba74aab2c503eff8
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-16 12:04:28 -0500

    otorgando valores para el header

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -3,14 +3,35 @@ import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import ButtonMultiple from "@/components/ButtonMultiple.vue";
 import { getSettingsPage } from "@/lib/get-settings-page";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 
-onMounted(async() => {
-    const settingsPage = await getSettingsPage();
-})
+const redes_sociales = ref([]);
+const contactos = ref([]);
+const logoudh = ref(null);
+const link_login = ref(null);
+
+const fetchSettings = async () => {
+    try {
+        const settingsPage = await getSettingsPage();
+
+        redes_sociales.value = settingsPage.redes_sociales || [];
+        contactos.value = settingsPage.contactos || [];
+        logoudh.value = settingsPage.logoudh || null;
+        link_login.value = settingsPage.link_login || null;
+    } catch (error) {
+        console.error("Error fetching settings data:", error);
+    }
+};
+
+onMounted(fetchSettings);
+
 </script>
 <template>
-    <Header />
+    <Header 
+        :redes_sociales="redes_sociales"
+        :logoudh="logoudh!"
+        :link_login="link_login!"
+    />
     <slot />
     <Footer />
     <ButtonMultiple />

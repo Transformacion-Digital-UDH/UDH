178b7962f5711adb8865e2abab823da6d1dacc0b
Renzo <renzopanduro00@gmail.com>
2024-11-22 12:33:03 -0500

    Strapi courses system

--- src/lib/carreras/sistemas/get-sistemas-carousel.ts
+++ src/lib/carreras/sistemas/get-sistemas-carousel.ts
@@ -1,6 +1,6 @@
 import { query } from "@/lib/strapi";
 
-export const getCarreraCarousel = async () => {
+export const getSistemasCarousel = async () => {
   return query(
     "carrera-sistema?fields[0]=locale&populate[imagenes_carousel][populate][imagen][fields]=url,name"
   ).then((res) => res.data);

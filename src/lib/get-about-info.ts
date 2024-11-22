56c55d6323f803c116bfccb99c7976dfa7779d31
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-11-22 12:07:38 -0500

    Actualizando data en About Us

--- src/lib/get-about-info.ts
+++ src/lib/get-about-info.ts
@@ -1,7 +1,7 @@
 import { query } from "@/lib/strapi"
 
 export const getAboutUs = async () => {
-    return query('home?fields[0]=locale&populate[nosotros][populate][beneficios][populate][icono][fields]=url,name&populate[nosotros][populate][imagen][fields]=url,name')
+    return query('home?fields[0]=locale&populate[nosotros][populate][beneficios][populate][icono][fields]=url,name&populate[nosotros][populate][imagen][fields]=url,name&populate[nosotros][populate][titulo_seccion][fields]=*')
         .then(res => {
             return res.data
         })

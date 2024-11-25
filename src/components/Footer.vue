1c75aa3944b50004e7c45d148ec6fbef523682a2
jeanbecerra <j.becerra.22.jhire@gmail.com>
2024-11-25 10:31:50 -0500

    NUEVOS AJUSTOS

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,14 +4,14 @@ import ButtonPrimarySecondEffect from '@/components/ButtonPrimarySecondEffect.vu
 import { ref, onMounted } from 'vue';
 import { getContactInfo } from '@/lib/get-contact-info';
 
-// definir variables para los datos referentes de los campos de la API
+// Variables para los datos de la API
 const contactos = ref([]);
 const logoudh_white = ref({});
 const enlaces_interes = ref([]);
 
 const baseApiUrl = import.meta.env.VITE_API_URL_STRAPI;
 
-// funcion para obtener los datos de la API y asignarlos a las variables
+// Función para obtener los datos de la API
 const fetchContactData = async () => {
     try {
         const contactData = await getContactInfo();
@@ -24,11 +24,10 @@ const fetchContactData = async () => {
     }
 };
 
-// Cargar datos al montar el componente
+// Montar los datos al cargar el componente
 onMounted(() => {
-    fetchContactData()
-})
-
+    fetchContactData();
+});
 </script>
 
 <template>
@@ -37,98 +36,97 @@ onMounted(() => {
         <div class="container mx-auto xs:mx-6 xl:mx-28">
             <!-- footer main -->
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 px-1 mt-14 mb-2 md:px-12 lg:px-2">
-                <!-- Information -->
+                <!-- Información -->
                 <div class="">
-                    <!-- logo -->
+                    <!-- Logo -->
                     <div class="flex lg:justify-start justify-center">
                         <img :src="`${baseApiUrl}${logoudh_white.url}`" :alt="logoudh_white.name" class="w-[250px] ">
                     </div>
                     <div class="my-6">
                         <h2 class="my-5">OFICINA CENTRAL</h2>
-                        <!-- Iterar sobre contactos -->
-                        <div v-for="(contacto, index) in contactos" :key="index" class="contact-item">
-                            <!-- Celular -->
-                            <a v-if="contacto.id === 15 ? contacto.celular : ''" href="#"
-                                class="flex my-3 gap-5 items-center content-center">
-                                <div class="items-center content-center bg-green-custom p-1 rounded-md">
-                                    <IconPhone size="19" />
-                                </div>
-                                <p>{{ contacto.celular }}</p>
-                            </a>
+                        <div v-for="(contacto, index) in contactos" :key="index">
+                            <template v-if="contacto.direccion === 'Jr. Hermilio Valdizán # 871 - Huánuco'">
+                                <!-- Celular -->
+                                <a v-if="contacto.celular" href="#"
+                                    class="flex my-3 gap-5 items-center content-center">
+                                    <div class="items-center content-center bg-green-custom p-1 rounded-md">
+                                        <IconPhone size="19" />
+                                    </div>
+                                    <p>{{ contacto.celular }}</p>
+                                </a>
 
-                            <!-- Dirección -->
-                            <a v-if="contacto.id === 15 ? contacto.direccion : ''" href="#"
-                                class="flex my-3 gap-5 items-center content-center">
-                                <div class="items-center content-center bg-green-custom p-1 rounded-md">
-                                    <IconMapPin size="20" />
-                                </div>
-                                <p>{{ contacto.direccion }}</p>
-                            </a>
+                                <!-- Dirección -->
+                                <a v-if="contacto.direccion" href="#"
+                                    class="flex my-3 gap-5 items-center content-center">
+                                    <div class="items-center content-center bg-green-custom p-1 rounded-md">
+                                        <IconMapPin size="20" />
+                                    </div>
+                                    <p>{{ contacto.direccion }}</p>
+                                </a>
 
-                            <!-- Email -->
-                            <a v-if="contacto.id === 15 ? contacto.correo_electronico : ''" href="#"
-                                class="flex my-3 gap-5 items-center content-center">
-                                <div class="items-center content-center bg-green-custom p-1 rounded-md">
-                                    <IconMail size="20" />
-                                </div>
-                                <p>{{ contacto.correo_electronico }}</p>
-                            </a>
+                                <!-- Email -->
+                                <a v-if="contacto.correo_electronico" href="#"
+                                    class="flex my-3 gap-5 items-center content-center">
+                                    <div class="items-center content-center bg-green-custom p-1 rounded-md">
+                                        <IconMail size="20" />
+                                    </div>
+                                    <p>{{ contacto.correo_electronico }}</p>
+                                </a>
+                            </template>
                         </div>
                     </div>
 
                     <div class="my-6">
                         <h2 class="my-5">CIUDAD UNIVERSITARIA</h2>
-                        <!-- Iterar sobre contactos -->
-                        <div v-for="(contacto, index) in contactos" :key="index" class="contact-item">
-                            <!-- Celular -->
-                            <a v-if="contacto.id === 16 ? contacto.celular : ''" href="#"
-                                class="flex my-3 gap-5 items-center content-center">
-                                <div class="items-center content-center bg-green-custom p-1 rounded-md">
-                                    <IconPhone size="19" />
-                                </div>
-                                <p>{{ contacto.celular }}</p>
-                            </a>
+                        <div v-for="(contacto, index) in contactos" :key="index">
+                            <template v-if="contacto.direccion === 'Carretera Central km 2.6 - Huánuco'">
+                                <!-- Celular -->
+                                <a v-if="contacto.celular" href="#"
+                                    class="flex my-3 gap-5 items-center content-center">
+                                    <div class="items-center content-center bg-green-custom p-1 rounded-md">
+                                        <IconPhone size="19" />
+                                    </div>
+                                    <p>{{ contacto.celular }}</p>
+                                </a>
 
-                            <!-- Dirección -->
-                            <a v-if="contacto.id === 16 ? contacto.direccion : ''" href="#"
-                                class="flex my-3 gap-5 items-center content-center">
-                                <div class="items-center content-center bg-green-custom p-1 rounded-md">
-                                    <IconMapPin size="20" />
-                                </div>
-                                <p>{{ contacto.direccion }}</p>
-                            </a>
+                                <!-- Dirección -->
+                                <a v-if="contacto.direccion" href="#"
+                                    class="flex my-3 gap-5 items-center content-center">
+                                    <div class="items-center content-center bg-green-custom p-1 rounded-md">
+                                        <IconMapPin size="20" />
+                                    </div>
+                                    <p>{{ contacto.direccion }}</p>
+                                </a>
 
-                            <!-- Email -->
-                            <a v-if="contacto.id === 16 ? contacto.correo_electronico : ''" href="#"
-                                class="flex my-3 gap-5 items-center content-center">
-                                <div class="items-center content-center bg-green-custom p-1 rounded-md">
-                                    <IconMail size="20" />
-                                </div>
-                                <p>{{ contacto.correo_electronico }}</p>
-                            </a>
+                                <!-- Email -->
+                                <a v-if="contacto.correo_electronico" href="#"
+                                    class="flex my-3 gap-5 items-center content-center">
+                                    <div class="items-center content-center bg-green-custom p-1 rounded-md">
+                                        <IconMail size="20" />
+                                    </div>
+                                    <p>{{ contacto.correo_electronico }}</p>
+                                </a>
+                            </template>
                         </div>
                     </div>
                 </div>
-                <!-- links -->
+                <!-- Enlaces de interés -->
                 <div class="pl-0 lg:pl-10 md:pl-12">
                     <h2
                         class="pb-4 relative before:contents-[''] before:absolute before:h-[3px] before:w-[90px] before:bottom-0 before:left-0 before:z-10 before:bg-gray-500 after:absolute after:contents-[''] after:w-[30px] after:h-[3px] after:bottom-0 after:left-5 after:z-10 after:bg-green-custom">
                         Enlaces de interés</h2>
 
                     <div class="mt-7">
-
                         <a :href="pf.link" v-for="(pf) in enlaces_interes" :key="pf.id"
                             class="flex my-3 gap-2 items-center content-center">
                             <div class="text-green-custom">
                                 <IconCaretRightFilled size="20" />
                             </div>
-                            <p>
-                                {{ pf.titulo }}
-                            </p>
+                            <p>{{ pf.titulo }}</p>
                         </a>
                     </div>
                 </div>
-                <!-- Newsletter -->
+                <!-- Boletín -->
                 <div
                     class="pl-0 lg:pl-16 md:pl-16 w-full md:w-full lg:w-full sm:w-3/5 md:col-span-2 lg:col-span-1 xl:w-full xl:pl-0">
                     <h2
@@ -138,7 +136,7 @@ onMounted(() => {
                     <div class="mt-7">
                         <p>
                             Suscríbete a nuestro boletín para no perderte
-                            las últimas noticias de la universidad y sus servicios.
+                            las últimas noticias de la universidad y sus servicios.
                         </p>
                         <form method="POST" action="#" class="mt-4">
                             <input type="email" required placeholder="Ingresa tu correo electrónico"
@@ -147,11 +145,8 @@ onMounted(() => {
                             <ButtonPrimarySecondEffect label="Enviar suscripción" class="!my-3" :icon="IconSend"
                                 type="submit" />
                         </form>
-
                     </div>
-
                 </div>
-
             </div>
 
             <!-- footer bottom-->
